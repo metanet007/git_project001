13790ad18b129c085e802ecc426fc0d94d5c8f44
Metin <mtnzmr@hotmail.com>
2020-08-08 14:06:41 -0600

    the third commit from local master

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -7,6 +7,9 @@ public class FirstClass {
 		System.out.println("our first commit from local repo");
 		System.out.println("our second commit from local repository");
 		System.out.println("now i am on my metin branch");
+		System.out.println("feature branch next commit");
+		System.out.println("i want to have conflict");
+		System.out.println("i want to have a second conflict");
 		
 
 
