8da582f8e15fbc3e42795db9c7ae0a8fc1765958
metin guclu <mtnzmr@hotmail.com>
2020-11-18 12:46:43 -0700

    first commit from local repository

---
+++ src/first_package/DenemeFeature.java
@@ -0,0 +1,11 @@
+package first_package;
+
+public class DenemeFeature {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ src/first_package/DenemeFirstClass.java
@@ -0,0 +1,11 @@
+package first_package;
+
+public class DenemeFirstClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("our furst commit from local repo");
+
+	}
+
+}
