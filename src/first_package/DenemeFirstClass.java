34f92c5da8cceb9f3c265998adef9434cceb212c
metin guclu <mtnzmr@hotmail.com>
2020-11-18 12:54:37 -0700

    my second commit from git repository

--- src/first_package/DenemeFirstClass.java
+++ src/first_package/DenemeFirstClass.java
@@ -2,9 +2,9 @@ package first_package;
 
 public class DenemeFirstClass {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println("our furst commit from local repo");
+		System.out.println("our second commit from local repository");
 
 	}
 
