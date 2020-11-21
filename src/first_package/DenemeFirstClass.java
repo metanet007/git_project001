43d0739995e193f21d76ee6ee6dc46fda82bb9ad
metin guclu <mtnzmr@hotmail.com>
2020-11-21 11:38:39 -0700

    my master branch

--- src/first_package/DenemeFirstClass.java
+++ src/first_package/DenemeFirstClass.java
@@ -5,6 +5,7 @@ public class DenemeFirstClass {
 	public static void main(String[] args) {		
 		System.out.println("our furst commit from local repo");
 		System.out.println("our second commit from local repository");
+		System.out.println("now i am on my master branch");
 
 	}
 
