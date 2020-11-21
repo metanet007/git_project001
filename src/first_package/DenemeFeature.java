04f38a4c46cb96c7994d0e2bec32ce49e77ca160
metanet007 <69377107+metanet007@users.noreply.github.com>
2020-11-21 12:38:59 -0700

    Update DenemeFeature.java
    second commit from remote

--- src/first_package/DenemeFeature.java
+++ src/first_package/DenemeFeature.java
@@ -5,6 +5,7 @@ public class DenemeFeature {
 	public static void main(String[] args){
 	System.out.println("this is my commit from feature branch");
 	System.out.println("this is my commit from remote feature branch");
+	System.out.println("this is my second commit from remote feature branch");
 		
 
 	}
